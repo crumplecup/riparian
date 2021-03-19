5cddc9b3fc65d56eb2e96f5c4365187e9f8471d4
crumplecup <erik.w.rose@gmail.com>
2021-03-18 18:49:53 -0700

    methods draft for peer review

--- data-raw/test.R
+++ data-raw/test.R
@@ -1,9 +1,83 @@
+blen <- 10
+bit <- rep(5, blen)
+tbit <- stats::fft(bit) / blen
+stats::fft(tbit, inverse = T)
+byt <- 1:blen
+tbyt <- stats::fft(byt) / blen
+ibyt <- stats::fft(tbyt, inverse = T) / blen
+tb <- tbyt + tbit
+itb <- stats::fft(tb, inverse = T)
+bat <- c(5, 5, 5, 5, 5)
+tbat <- stats::fft(bat) / 5
+tb <- tbit * tbat
+itb <- stats::fft(tb, inverse = T) / 5
+plot(abs(itb))
 
-ortho20 <- '/media/crumplecup/catacomb/gis/benton20_3in'
+rec <- data.table::fread('/home/erik/output/df20k_1000.csv')
+recf <- data.table::fread('/home/erik/output/ff20k_1000.csv')
+recg <- data.table::fread('/home/erik/output/fg20k_1000.csv')
+# AD test
+plot(rec$input, rec$ad, pch = 20, col = get_palette('charcoal'),
+     xlab = 'rate', ylab = 'test statistic')
+points(recf$input, recf$ad, col = get_palette('crimson'), pch = 20)
+points(recg$input, recg$ad, col = get_palette('ocean'), pch = 20)
+
+# Chi-squared
+plot(rec$input, rec$chs, pch = 20, col = get_palette('charcoal'),
+     xlab = 'rate', ylab = 'test statistic')
+points(recf$input, recf$chs, col = get_palette('crimson'), pch = 20)
+points(recg$input, recg$chs, col = get_palette('ocean'), pch = 20)
+
+# KS test
+plot(rec$input, rec$ks, pch = 20, col = get_palette('charcoal'),
+     xlab = 'rate', ylab = 'test statistic')
+points(recg$input, recg$ks, col = get_palette('ocean'), pch = 20)
+points(recf$input, recf$ks, col = get_palette('crimson'), pch = 20)
+legend('topright', legend = c('debris flows', 'fines', 'gravels'), 
+       fill = get_palette(c('charcoal', 'crimson', 'ocean'), .8))
+
+# Kuiper test
+plot(rec$input, rec$kp, pch = 20, col = get_palette('charcoal'),
+     xlab = 'rate', ylab = 'test statistic')
+points(recg$input, recg$kp, col = get_palette('ocean'), pch = 20)
+points(recf$input, recf$kp, col = get_palette('crimson'), pch = 20)
+
+
+d <- 0.1 / 0.237
+pwr <- seq(0, .99, .01)
+n <- unlist(lapply(pwr, function(x) power.t.test(d= d, power = x)$n))
+
+png('monitoring_stat_pwr.png', height = 17, width = 25, units = 'cm', res = 300)
+plot(n, pwr, type = 'l', col = get_palette('ocean'), lwd = 3,
+     xlab = '# of samples', ylab = 'statistical power',
+     main = 'power to detect mean cover change of 10% in priority corridors')
+abline(v = 54, lty = 2, col = get_palette('coral'), lwd = 2)
+pts <- matrix(c(54, 0.59), ncol = 2)
+abline(v = 89, lty = 2, col = get_palette('charcoal'), lwd = 2)
+pts <- rbind(pts, c(89, 0.8))
+abline(v = 119, lty = 2, col = get_palette('gold'), lwd = 2)
+pts <- rbind(pts, c(119, 0.9))
+text(60, .59, '54')
+text(95, .8, '89')
+text(126, .9, '119')
+points(pts, col = get_palette(c('coral', 'charcoal', 'gold'), .7))
+legend('bottomright', legend = c('59% power (2018 level)', '80% power', '90% power'),
+       fill = get_palette(c('coral', 'charcoal', 'gold'), .7))
+dev.off()
+
+plot(sf::st_zm(random_samples[33,]))
+
+
+
+load('/home/erik/data/permit_samples_144.rds')
+
+ortho20 <- '/media/erik/catacomb/gis/benton20_3in'
 ortho20b <- '/media/crumplecup/catacomb/gis/benton20_9in'
 ortho18 <- '/media/crumplecup/catacomb/gis/benton_2018'
 
-sam_path <- '/home/crumplecup/work/samples'
+sam_path <- '/home/erik/output/thumbs'
+# dir.create('/home/erik/output/perm_plots')
+perm_path <- '/home/erik/output/perm_plots'
 
 # annual thumbnail paths
 thumb_20 <- file.path(sam_path, 'annual2020/thumb20_3in')
@@ -16,21 +90,22 @@ out_20b <- file.path(sam_path, 'permit2020/2020_9in')
 out_18 <- file.path(sam_path, 'permit2020/2018')
 
 # write thumbnails to drive
-thumbnails(ortho20, thumb_20, random_samples)
+thumbnails(ortho20, sam_path, permit_samples)
 thumbnails(ortho20b, thumb_20b, random_samples)
 thumbnails(ortho18, thumb_18, random_samples)
 
 # plot sample boxes
+plot_samples(sam_path, '/home/erik/output', permit_samples, 2020, method = 'lm3')
 plot_samples(thumb_20, out_20, random_samples, 2020, method = 'lm3')
 plot_samples(thumb_20b, out_20b, random_samples, 2020, method = 'lm3')
 plot_samples(thumb_18, out_18, random_samples, 2018, method = 'lm')
 
 library(data.table)
-perms <- fread('permits20.csv')
+perms <- data.table::fread('/home/erik/data/permits20.csv')
 perm_mtls <- perms$maptaxlot
 perm_lots <- sf_lots[sf_lots$MapTaxlot %in% perm_mtls, ]
 
-permit_samples <- sample_streams(75, lots = perm_lots)
+permit_samples <- sample_streams(3, lots = perm_lots)
 # screen out bad boxes [omitted]
 # save(permit_samples, file = 'permit_samples.rds')
 
